7247633e47d9ed53bc72952594f4462396a9f566
INDOX0D3 <107053080+INDOX0D3@users.noreply.github.com>
2022-09-11 21:20:04 +0700

    Update index.php

--- fix/index.php
+++ fix/index.php
@@ -71,7 +71,7 @@
                 <li class="sidebar-title">Menu</li>
 
                 <li class="sidebar-item active">
-                    <a href="https://urania.icu" class='sidebar-link'>
+                    <a href="#" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
