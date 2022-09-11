c0b1222ec94dbc0b2b49409737209269e305e3ef
INDOX0D3 <107053080+INDOX0D3@users.noreply.github.com>
2022-09-11 21:33:00 +0700

    Update index.php

--- fix/index.php
+++ fix/index.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://urania.icu/assets/css/shared/iconly.css">
     <link rel="stylesheet" href="https://urania.icu/assets/css/pages/fontawesome.css">
     <link rel="stylesheet" href="https://urania.icu/assets/css/pages/datatables.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/npm/toastify-js@1.12.0/src/toastify.css">
 
 </head>
 
